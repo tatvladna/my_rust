a5d019c9bb658afbd8a7096ee1d55438e66663c6
tatvladna <tpustovalova@mail.ru>
2025-03-11 15:10:35 +0300

    update

--- main.rs
+++ main.rs
@@ -12,9 +12,37 @@ fn main() {
     // затенение 
     let id: u32 = 555;
     println!("id: {}", id);
-    let id: u32 = 1;
-    println!("id: {}", id);
     let id: &str = "12345";
     println!("id: {}", id);
+    let id: u32 = 1;
+    println!("id: {}", id);
 
+    let num = if id == 5 {10} else {0};
+    println!("num: {}", num);
+
+
+    let res: &str = match num {
+        0 => "Zero",
+        5 => "Five",
+        10 => "Ten",
+        _ => "непонятно",
+    };
+    println!("res = {}", res);
+
+    s(id);
+    println!("Рузультат все равно не поменялся = {}", id);
+
+    let res2 = q(id);
+    println!("res2 = {}", res2);
+}
+
+fn s(mut n: u32)  {
+    println!("до = {}", n);
+    n *= 10;
+    println!("после = {}", n);
+}
+
+
+fn q(n:u32) -> u32 {
+    return n + 50;
 }
