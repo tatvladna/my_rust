175d90865adec4da12f934deb39e1a66d6f317c9
tatvladna <tpustovalova@mail.ru>
2025-03-24 23:32:16 +0300

    updated

--- src/ownership_borrowing.rs
+++ src/ownership_borrowing.rs
@@ -1,3 +1,8 @@
 pub fn ownership_borrowing() {
     println!("============ Владение и заимствование ===============");
+
+    {
+        let tmp = -70;
+        println!("Внутренний scope: tmp = {}", tmp); 
+    }
 }
