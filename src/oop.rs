13a4677dbf5e47f4fff2ffe7c4e555aaffe8d200
tatvladna <tpustovalova@mail.ru>
2025-03-25 13:03:24 +0300

    updated

--- src/oop.rs
+++ src/oop.rs
@@ -1,3 +1,22 @@
 pub fn oop() {
     println!("============== ООП ===============");
+    let tanya = Person::new("Tanya", 23);
+    println!("Имя: {}, Возраст: {}", tanya.name, tanya.age);
+
+}
+
+
+struct Person {
+    name: String,
+    age: u32,
+}
+
+// методы
+impl Person {
+    fn new(name: &str, age: u32) -> Self {
+        Self {
+            name: name.to_string(),
+            age: age,
+        }
+    }
 }
